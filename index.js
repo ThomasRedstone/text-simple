1fd768db719b2d657cb0bc84ddfebd8742cc835b
Thomas Redstone <tom@redstonetechnology.net>
2019-12-17 15:17:56 +0000

    Update to fix reporter to match the format expected with the version of Jest being used

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
 /*
  Copyright 2012-2015, Yahoo Inc.
  Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
- Based on the
  */
-'use strict';
-const { ReportBase } = require('istanbul-lib-report');
+"use strict";
 
-class TextSimpleReport extends ReportBase {
-    constructor(opts) {
-        super();
-
-        opts = opts || {};
-        this.file = opts.file || null;
-    }
-
-    onStart(node, context) {
-        const summary = node.getCoverageSummary();
-        const cw = context.writer.writeFile(this.file);
-        const printLine = function(key) {
-            const str = lineForKey(summary, key);
-            const clazz = context.classForPercent(key, summary[key].pct);
-            cw.println(cw.colorize(str, clazz));
-        };
-
-        const checks = Object.keys(summary);
-        if(checks.reduce((accumulator, currentValue) => ((accumulator + currentValue))) / checks.length === 100) {
-            cw.println('100% coverage');
-        }
-        else {
-            printLine('statements');
-            printLine('branches');
-            printLine('functions');
-            printLine('lines');
-        }
-
-        cw.close();
-    }
+function TextSimpleReport(opts) {
+    opts = opts || {};
+    this.file = opts.file || "text-simple.txt";
 }
 
 function lineForKey(summary, key) {
@@ -43,21 +14,48 @@ function lineForKey(summary, key) {
 
     key = key.substring(0, 1).toUpperCase() + key.substring(1);
     if (key.length < 12) {
-        key += '                   '.substring(0, 12 - key.length);
+        key += "                   ".substring(0, 12 - key.length);
     }
     const result = [
         key,
-        ':',
-        metrics.pct + '%',
-        '(',
-        metrics.covered + '/' + metrics.total,
-        ')'
-    ].join(' ');
+        ":",
+        metrics.pct + "%",
+        "(",
+        metrics.covered + "/" + metrics.total,
+        ")"
+    ].join(" ");
     const skipped = metrics.skipped;
     if (skipped > 0) {
-        return result + ', ' + skipped + ' ignored';
+        return result + ", " + skipped + " ignored";
     }
     return result;
 }
+
+TextSimpleReport.prototype.onStart = function(node, context) {
+    const summary = node.getCoverageSummary();
+    const cw = context.writer.writeFile(this.file);
+    const printLine = function(key) {
+        const str = lineForKey(summary, key);
+        const clazz = context.classForPercent(key, summary[key].pct);
+        cw.println(cw.colorize(str, clazz));
+    };
+
+    const checks = Object.keys(summary.data);
+    if (
+        checks.reduce(
+            (accumulator, currentValue) =>
+                accumulator + summary.data[currentValue].pct,
+            0
+        ) / checks.length === 100
+    ) {
+        cw.println("100% coverage");
+    } else {
+        printLine("statements");
+        printLine("branches");
+        printLine("functions");
+        printLine("lines");
+    }
+    cw.close();
+};
 
 module.exports = TextSimpleReport;
